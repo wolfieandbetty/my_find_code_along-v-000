e03b5d20fffde2cd772395ecbb23400de50e0bfb
Cat Norris <cannenorris@yahoo.com>
2017-12-01 04:49:06 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 #Iterate through each element in a passed argument (collection) using while. Use a counter (i) and increment each
 #time you go into the loop
-
-def my_find(collection=[1,5,12,15,21])
+collection=[1,5,15,21,45]
+def my_find (collection)
   i = 0
   while i < collection.length
     return collection[i] if yield(collection[i]) # when the method hits this line it sends the value of collection[i] to the block
