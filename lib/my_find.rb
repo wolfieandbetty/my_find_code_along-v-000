0c8d8fab09d867d349ed7e87d0f0ca5e2b52eb2b
Cat Norris <cannenorris@yahoo.com>
2017-12-01 04:53:36 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -7,7 +7,7 @@ collection=(1..100).to_a
 def my_find (collection)
   i = 0
   while i < collection.length
-    return collection[i] if yield(collection[i]) # when the method hits this line it sends the value of collection[i] to the block
+    return collection[i] if yield(collection[i]) 
     i+=1
   end
 end
