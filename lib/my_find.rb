6ad17a33e72234691fefdcb0e509c334ebe976cb
Cat Norris <cannenorris@yahoo.com>
2017-12-01 04:52:05 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 #Iterate through each element in a passed argument (collection) using while. Use a counter (i) and increment each
 #time you go into the loop
-collection=(1.100).to_a
+collection=(1..100).to_a
 
 def my_find (collection)
   i = 0
